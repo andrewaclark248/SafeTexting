063ea15acd2a0e3c50424a2060a245ccf75bcea0
andrewaclark248 <andrewaclark248@gmail.com>
2023-07-29 19:39:34 -0400

    add default ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Grid } from '@mui/material'
-import LoginForm from './components/login/LoginForm'
-import { createTheme, ThemeProvider, styled } from '@mui/material';
-import safeLoginsLogo from "./assets/safe-logins-logo.png"
-import safeTextingLogo from "./assets/safe-texting-logo.png"
-const theme = createTheme({
+//import { Grid } from '@mui/material'
+//import LoginForm from './components/login/LoginForm'
+//import { createTheme, ThemeProvider, styled } from '@mui/material';
+//import safeLoginsLogo from "./assets/safe-logins-logo.png"
+//import safeTextingLogo from "./assets/safe-texting-logo.png"
+
+/***const theme = createTheme({
   palette: {
     mode: 'light',
     primary: {
@@ -17,32 +18,27 @@ const theme = createTheme({
     }
   }
 
-});
+});***/
 
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-
-      <div >
-
-        <Grid container spacing={0} sx={{ mt: 10 }} >
-          <Grid item xs={1}>
-          </Grid>
-          <Grid item xs={5}>
-            <img src={safeTextingLogo} style={{ width: "100%", height: "100%" }}/>
-          </Grid>
-          <Grid item xs={5}>
-            <LoginForm />
-          </Grid>
-          <Grid item xs={1}>
-          </Grid>
-
-        </Grid>
-
-      </div>
-
-    </ThemeProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
