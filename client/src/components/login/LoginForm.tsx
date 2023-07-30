bb35af916668663801fed541ee179ae7fde4c7da
andrewaclark248 <andrewaclark248@gmail.com>
2023-07-29 20:05:18 -0400

    partial card

--- client/src/components/login/LoginForm.tsx
+++ client/src/components/login/LoginForm.tsx
@@ -1,6 +1,5 @@
 
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
@@ -16,30 +15,6 @@ function LoginForm() {
           <Typography gutterBottom variant="h5" component="div" align="center">
             Login
           </Typography>
-
-          <Grid container spacing={0} sx={{ mt: 5 }} >
-            <Grid item xs={12}>
-              <TextField label={'Username'} id="margin-dense" margin="dense" fullWidth/>
-            </Grid>
-          </Grid>
-          <Grid container spacing={0}  >
-            <Grid item xs={12}>
-              <TextField label={'Password'} id="margin-dense" margin="dense" fullWidth/>
-            </Grid>
-          </Grid>
-          <Grid container spacing={0} sx={{ mt: 5 }} >
-            <Grid item xs={12}>
-              <Button variant="contained" fullWidth>Login</Button>
-            </Grid>
-          </Grid>
-          <Grid container spacing={2} sx={{ mt: 1 }} >
-            <Grid item xs={6}>
-              <Button variant="contained" color="error" fullWidth>SignUp</Button>
-            </Grid>
-            <Grid item xs={6}>
-              <Button variant="contained" color="error" fullWidth>Forget Your Creds?</Button>
-            </Grid>
-          </Grid>
 
 
         </CardContent>
