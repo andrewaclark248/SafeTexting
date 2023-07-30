5931c104071de14d83e17fce0ec7c73f3e8969a4
andrewaclark248 <andrewaclark248@gmail.com>
2023-07-29 22:24:57 -0400

    add image

--- client/src/components/login/LoginForm.tsx
+++ client/src/components/login/LoginForm.tsx
@@ -19,6 +19,9 @@ function LoginForm() {
           <Grid container spacing={2}>
             <Grid item xs={1}>
             </Grid>
+            <Grid item xs={5}>
+              <img src={safeTextingLogo} style={{ width: "100%", height: "100%" }}/>
+            </Grid>
           </Grid>
 
 
