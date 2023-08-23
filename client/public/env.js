dcc0392d871296b6678f0ba8cdebb4d7456c7f91
andrewaclark248 <andrewaclark248@gmail.com>
2023-08-22 23:01:02 -0400

    various changes

--- client/public/env.js
+++ client/public/env.js
@@ -58,5 +58,5 @@ window.env = {
   "FIREBASE_PROJECT_ID": "maskedtexting-staging",
   "FIREBASE_STORAGE_BUCKET": "maskedtexting-staging.appspot.com",
   "FIREBASE_MESSAGING_SENDER_ID": "1007088319970",
-  "FIREBASE_APP_ID": "G-LY0DPFS8DL"
+  "FIREBASE_APP_ID": "G-1:1007088319970:web:1ef183658c01ba210c4dbb"
 };
