7e30c722c2cccd8118101a06a8d28186096eeda3
andrewaclark248 <andrewaclark248@gmail.com>
2023-11-19 21:23:12 -0500

    add modal

--- client/src/components/profile/ResetPassword.tsx
+++ client/src/components/profile/ResetPassword.tsx
@@ -9,7 +9,9 @@ import Grid from '@mui/material/Grid';
 import InfoIcon from '@mui/icons-material/Info';
 import Tooltip from '@mui/material/Tooltip';
 import IconButton from '@mui/material/IconButton';
-
+import Modal from '@mui/material/Modal';
+import {useState} from 'react'
+import Box from '@mui/material/Box'
 
 function ResetPassword() {
 
@@ -31,7 +33,7 @@ function ResetPassword() {
                         </Tooltip>
                     </Grid>
                     <Grid item xs={6}>
-                        <Button variant="contained">Reset Password</Button>
+                        <ResetPasswordModal />
                     </Grid>
                     <Grid item xs={6}>
                         Change Email
@@ -62,5 +64,45 @@ function ResetPassword() {
         </Card>
     );
 }
+
+function ResetPasswordModal() {
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+  
+    const style = {
+        position: 'absolute' as 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+      };
+
+
+    return (
+      <div>
+        <Button variant="contained"  onClick={handleOpen}>Reset Password</Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Reset Password
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+        </Modal>
+      </div>
+    );
+  }
 
 export default ResetPassword;
