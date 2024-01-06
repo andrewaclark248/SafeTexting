a4ad920c54a42ad5fbbe7d9a3fb566e1e2784436
andrewaclark248 <andrewaclark248@gmail.com>
2024-01-05 23:15:04 -0500

    update env vars

--- client/public/env.js
+++ client/public/env.js
@@ -1,3 +1,9 @@
 window.env = {
-  "FIREBASE_API_KEY": "AIzaSyDJLmlqOtzoBWEslT82sFMgmnyb7Or1uy8"
+  "FIREBASE_API_KEY": "AIzaSyDJLmlqOtzoBWEslT82sFMgmnyb7Or1uy8",
+  "FIREBASE_AUTH_DOMAIN": "maskedtexting-staging.firebaseapp.com",
+  "FIREBASE_PROJECT_ID": "maskedtexting-staging",
+  "FIREBASE_STORAGE_BUCKET": "maskedtexting-staging.appspot.com",
+  "FIREBASE_MESSAGING_SENDER_ID": "1007088319970",
+  "FIREBASE_APP_ID": "G-1:1007088319970:web:1ef183658c01ba210c4dbb",
+  "SAFE_TEXTING_API": "http://localhost:5000"
 };
