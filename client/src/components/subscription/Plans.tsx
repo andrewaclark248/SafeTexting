e6479ace5306c9ac4da98c55db28decb8599bc87
andrewaclark248 <andrewaclark248@gmail.com>
2024-02-03 21:06:36 -0500

    show plans

--- client/src/components/subscription/Plans.tsx
+++ client/src/components/subscription/Plans.tsx
@@ -1,43 +1,23 @@
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
 import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-
+import Gold from './Gold'
+import Bronze from './Bronze'
 
 function Plans(props: any) {
 
     return (
-        <>
-
-
-            <Card variant="outlined" >
-                <CardContent>
-
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            <Typography sx={{ fontSize: 18, fontWeight: "bold" }} color="text.primary" gutterBottom>
-                                Subscriptions
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <Typography sx={{ fontSize: 12, fontWeight: "bold" }} color="text.primary" gutterBottom>
-                                Gold
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <Button variant="contained">Purchase Gold</Button>
-                        </Grid>
-                    </Grid>
-
-
-
-
-                </CardContent>
-            </Card>
-
-
-        </>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+            <Gold />
+        </Grid>
+        <Grid item xs={6}>
+            <Bronze />
+        </Grid>
+        <Grid item xs={6}>
+        </Grid>
+        <Grid item xs={6}>
+        </Grid>
+      </Grid>
+  
     );
 
 }
